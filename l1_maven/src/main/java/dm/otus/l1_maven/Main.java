e3a5dc4ff985c0f0c78de25c7cd19936050b1018
demen777 <demen7@gmail.com>
2017-10-15 07:45:27 +0500

    Use List instead Collection Fix pom

--- l1_maven/src/main/java/dm/otus/l1_maven/Main.java
+++ l1_maven/src/main/java/dm/otus/l1_maven/Main.java
@@ -3,12 +3,12 @@ package dm.otus.l1_maven;
 import com.google.common.collect.ImmutableSet;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Collection<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         Random rand = new Random();
         for (int i = 0; i < 999_999 + 1; i++) {
             list.add(rand.nextInt(100000));
