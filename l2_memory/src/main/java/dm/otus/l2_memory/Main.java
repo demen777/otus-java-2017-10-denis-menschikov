9ea9b7f46136c3f1849243417b87f314706be026
demen777 <demen7@gmail.com>
2017-10-22 10:16:32 +0500

    Remove static, add final for constants

--- l2_memory/src/main/java/dm/otus/l2_memory/Main.java
+++ l2_memory/src/main/java/dm/otus/l2_memory/Main.java
@@ -3,18 +3,19 @@ package dm.otus.l2_memory;
 public class Main {
     public static void main(String[] args)
             throws InstantiationException, IllegalAccessException, InterruptedException, ClassNotFoundException {
+        SizeCalculator sizeCalculator = new SizeCalculator();
         String[] classNames = {"java.lang.Object", "java.lang.String", "dm.otus.l2_memory.Main"};
         for(String className: classNames) {
-            System.out.printf("Class %s has size %d bytes%n", className, SizeCalculator.getSizeByName(className));
+            System.out.printf("Class %s has size %d bytes%n", className, sizeCalculator.getSizeByName(className));
         }
         for(int length=0; length < 11; length++){
             Object[] array = new Object[length];
             System.out.printf("Object[%d] has size %d bytes%n", length,
-                    SizeCalculator.getArraySizeInBytes(array));
+                    sizeCalculator.getArraySizeInBytes(array));
         }
         for(int length=0; length < 11; length++){
             System.out.printf("String lenght=%d has size  %d bytes%n", length,
-                    SizeCalculator.getStringSizeInBytes(length));
+                    sizeCalculator.getStringSizeInBytes(length));
         }
     }
 }
--- l2_memory/src/main/java/dm/otus/l2_memory/SizeCalculator.java
+++ l2_memory/src/main/java/dm/otus/l2_memory/SizeCalculator.java
@@ -1,24 +1,24 @@
 package dm.otus.l2_memory;
 
 public class SizeCalculator {
-    public static long getSizeByName(String className)
+    public long getSizeByName(String className)
             throws ClassNotFoundException, IllegalAccessException, InstantiationException, InterruptedException {
         Class class_ = Class.forName(className);
         int quantityObjects = 1_000_000;
         Object[] data = new Object[quantityObjects];
         long usedAfter;
         int i;
-        long usedBefore = getUsedMemory();
+        final long usedBefore = getUsedMemory();
         for(i=0; i<quantityObjects; i++){
             data[i] = "java.lang.String".equals(className) ? new String(new char[0]) : class_.newInstance();
         }
         usedAfter = getUsedMemory();
-        long res = Math.round((usedAfter-usedBefore)/(double)quantityObjects);
+        final long res = Math.round((usedAfter-usedBefore)/(double)quantityObjects);
         System.out.println(data[0]);
         return res;
     }
 
-    public static long getArraySizeInBytes(Object[] array) throws InterruptedException {
+    public long getArraySizeInBytes(Object[] array) throws InterruptedException {
         long usedAfter;
         long usedBefore;
         usedBefore = getUsedMemory();
@@ -28,17 +28,16 @@ public class SizeCalculator {
         return usedAfter-usedBefore;
     }
 
-    public static long getStringSizeInBytes(int length) throws InterruptedException {
+    public long getStringSizeInBytes(int length) throws InterruptedException {
         long usedAfter;
-        long usedBefore;
-        usedBefore = getUsedMemory();
-        String str = new String(new char[length]);
+        final long usedBefore = getUsedMemory();
+        final String str = new String(new char[length]);
         usedAfter = getUsedMemory();
         System.out.println(str);
         return usedAfter-usedBefore;
     }
 
-    private static long getUsedMemory() throws InterruptedException {
+    private long getUsedMemory() throws InterruptedException {
         Runtime runtime = Runtime.getRuntime();
         System.gc();
         Thread.sleep(1000);
