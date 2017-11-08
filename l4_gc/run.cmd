bd0f254897f6ab5223538c98bf0c73f411d841a4
demen <demen7@gmail.com>
2017-11-08 22:58:30 +0500

    Uncomment MemoryLeaker run in script

--- l4_gc/run.cmd
+++ l4_gc/run.cmd
@@ -1,14 +1,14 @@
 set JAVAMEMORY=-Xms512m -Xmx512m
 set JAVAGC=-verbose:gc -XX:+PrintGCDateStamps -XX:+PrintGCDetails
 set GCLOGFILENAME=gc_serial.log
-rem java %JAVAMEMORY% %JAVAGC%  -Xloggc:%GCLOGFILENAME% -XX:+UseSerialGC -cp target/classes dm.otus.l4_gc.Main
+java %JAVAMEMORY% %JAVAGC%  -Xloggc:%GCLOGFILENAME% -XX:+UseSerialGC -cp target/classes dm.otus.l4_gc.Main
 java -cp target/classes dm.otus.l4_gc.GCLogParser %GCLOGFILENAME%
 set GCLOGFILENAME=gc_parallel.log
-rem java %JAVAMEMORY% %JAVAGC% -Xloggc:%GCLOGFILENAME% -XX:+UseParallelGC -XX:+UseParallelOldGC -cp target/classes dm.otus.l4_gc.Main
+java %JAVAMEMORY% %JAVAGC% -Xloggc:%GCLOGFILENAME% -XX:+UseParallelGC -XX:+UseParallelOldGC -cp target/classes dm.otus.l4_gc.Main
 java -cp target/classes dm.otus.l4_gc.GCLogParser %GCLOGFILENAME%
 set GCLOGFILENAME=gc_concurrent.log
-rem java %JAVAMEMORY% %JAVAGC%  -Xloggc:%GCLOGFILENAME% -XX:+UseConcMarkSweepGC -cp target/classes dm.otus.l4_gc.Main
+java %JAVAMEMORY% %JAVAGC%  -Xloggc:%GCLOGFILENAME% -XX:+UseConcMarkSweepGC -cp target/classes dm.otus.l4_gc.Main
 java -cp target/classes dm.otus.l4_gc.GCLogParser %GCLOGFILENAME%
 set GCLOGFILENAME=gc_g1.log
-rem java %JAVAMEMORY% %JAVAGC%  -Xloggc:%GCLOGFILENAME% -XX:+UseG1GC -cp target/classes dm.otus.l4_gc.Main
+java %JAVAMEMORY% %JAVAGC%  -Xloggc:%GCLOGFILENAME% -XX:+UseG1GC -cp target/classes dm.otus.l4_gc.Main
 java -cp target/classes dm.otus.l4_gc.GCLogParser %GCLOGFILENAME%
